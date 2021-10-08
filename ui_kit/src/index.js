34ef48c870511b54b0eaef30bb28deaa3f4af30e
Alexandr Dyomin <alexandr.dyomin91@gmail.com>
2021-10-08 13:22:01 +0900

    Начал использовать SCSS

---
+++ ui_kit/src/index.js
@@ -0,0 +1,5 @@
+import "../public/index.scss";
+import "./blocks/color-card/color-card.scss";
+import "./blocks/font/font.scss";
+import "./blocks/header/header.scss";
+import "./blocks/container/container.scss";
