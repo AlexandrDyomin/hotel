8673953bb67ed4c2db433fedfe2202255de3e274
Alexandr Dyomin <alexandr.dyomin91@gmail.com>
2021-10-12 21:08:51 +0700

    Добавил в обработчик события сокрытие выпадающего списка

--- ui_kit/src/blocks/dropdown/dropdown.js
+++ ui_kit/src/blocks/dropdown/dropdown.js
@@ -1,8 +1,8 @@
 const handleDropdownListFocus = ( e ) => {
-  console.log( "Событие произошло на элементе " + e.target );
-  console.log( "Обработчик сработал на элементе " + e.currentTarget );
-  console.log( "Обработчик сработал на фазе " + e.eventPhase );
-  console.log(e);
+  let elem = document.querySelector(".dropdown-list__bottom");
+  heightElem = elem.offsetHeight;
+  elem.style.transform=`translateY( -${ heightElem }px )`
+  // console.log(heightElem);
  
 }
 
