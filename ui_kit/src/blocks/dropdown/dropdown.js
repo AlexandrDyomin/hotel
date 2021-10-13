73a834cb057e7b58719b8abccd574f041f64cb10
Alexandr Dyomin <alexandr.dyomin91@gmail.com>
2021-10-13 13:27:03 +0900

    Добавил обработчики для показа и скрытия списка

--- ui_kit/src/blocks/dropdown/dropdown.js
+++ ui_kit/src/blocks/dropdown/dropdown.js
@@ -1,10 +1,42 @@
-const handleDropdownListFocus = ( e ) => {
-  let elem = document.querySelector(".dropdown-list__bottom");
-  heightElem = elem.offsetHeight;
-  elem.style.transform=`translateY( -${ heightElem }px )`
-  // console.log(heightElem);
- 
+const handleTextFieldFocus = ( e ) => {
+  let elem = document.querySelector( ".dropdown-list__bottom" );
+  elem.style.transform="translateY( 0% )";
+
+  elem = document.querySelector( ".dropdown-list" ).children[0];
+  elem.classList.add( "text-field_border-bottom-radius_disabled" );
+  elem.style.borderColor="#1F204180";
 }
 
-let elem = document.querySelector( ".dropdown-list > .text-field" );
-elem.addEventListener( "focus", handleDropdownListFocus );
+const handleTextFieldBlur = ( e ) => {
+  let elem = document.querySelector( ".dropdown-list__bottom" );
+  elem.style.transform=`translateY( -100% )`;
+
+  elem = document.querySelector( ".dropdown-list" ).children[0];
+  elem.classList.remove( "text-field_border-bottom-radius_disabled" );
+  elem.style.borderColor="#1F204140";
+}
+
+const handleContainerMouseDown = ( e ) => {
+  let elem = document.querySelector( ".dropdown-list" ).children[0];
+  elem.removeEventListener( 'blur', handleTextFieldBlur );
+}
+
+const handleContainerMouseUp = ( e ) => {
+  let elem = document.querySelector( ".dropdown-list" ).children[0];
+  elem.addEventListener( 'blur', handleTextFieldBlur );
+  elem.focus();
+}
+
+let elem = document.querySelector( ".dropdown-list" ).children[0];
+elem.addEventListener( "focus", handleTextFieldFocus );
+elem.addEventListener( "blur", handleTextFieldBlur );
+
+elem = document.querySelector( ".dropdown-list__container" );
+elem.addEventListener( "mousedown", handleContainerMouseDown );
+elem.addEventListener( "mouseup", handleContainerMouseUp );
+
+
+
+
+
+
