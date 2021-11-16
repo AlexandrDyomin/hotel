a7c29d7c967828e6dfcd8eb90ed5598cfb59c187
Alexandr Dyomin <alexandr.dyomin91@gmail.com>
2021-11-16 11:24:17 +0900

    Решил проблему с изменением цвета границы при переключении кнопки

--- ui_kit/src/blocks/toggle-button/toggle-button.js
+++ ui_kit/src/blocks/toggle-button/toggle-button.js
@@ -2,7 +2,7 @@ import addHandler from "../../common-modules/addHandler.js";
 import { changeAppearance } from "../counter/counter.js";
 
 const handleRadioChange = ( e ) => {
-  changeAppearance( e.currentTarget, "toggle-button_off");
+  changeAppearance( e.currentTarget, "toggle-button_on");
 }
 
 addHandler(
