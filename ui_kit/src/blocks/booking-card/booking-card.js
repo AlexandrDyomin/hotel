62cd6a4731415ba8e03d9ee638f1548c1d70a34f
Alexandr Dyomin <alexandr.dyomin91@gmail.com>
2021-11-30 20:59:21 +0900

    Закончил верстать карточку бронирования номера

--- ui_kit/src/blocks/booking-card/booking-card.js
+++ ui_kit/src/blocks/booking-card/booking-card.js
@@ -7,6 +7,7 @@ import {
   makeHandlerOnHide 
 } from "../date-dropdown/date-dropdown.js";
 import { findParent } from "../../common-modules/scan";
+import addHandler from "../../common-modules/addHandler";
 
 // возвращает дату в формате ISO
 // принимает в качестве аргумента строку в формате дд.мм.гггг
@@ -16,6 +17,27 @@ const getISODate = str => str.slice( 6, 10 ) + "." + str.slice( 3, 6 ) + str.sli
 const getFormattedPrice = str => ( str.length > 3) ?
   str.slice( 0, str.length - 3 ) + " " + str.slice( str.length - 3 )
   : str;
+
+// возвращает слово сутки в нужном склонении
+const getLastWord = ( numberDays ) => ( numberDays > 1 ) ? " суток" : " сутки";
+
+// возвращает число гостей исключая младенцев
+const getNumberGuests = () => {
+  let countersValues = $bookingCard.querySelectorAll( ".counter__display" );
+  let numberGuests = 0;
+  countersValues.forEach( ( display, index ) => {
+   if ( index !== 2 ) numberGuests += +display.innerText
+  });
+  return numberGuests;
+}
+// пересчитывает цену аренды
+const recalculatePrice = () => {
+  let result = ( pricePerDay * numberDays );
+  $result.innerText = getFormattedPrice( result.toString() );
+
+  let total = result - discount + serviceCharge + additionalServiceCharge;
+  $total.innerText = getFormattedPrice( total.toString() );
+}
 
 let settings = { ... defaultSettings };
 settings.container = ".booking-card__date-dropdown";
@@ -32,9 +54,8 @@ dp.selectDate( [ dateFrom, dateTo ] );
 
 let numberDays = ( dateTo.getTime() - dateFrom.getTime() ) / ( 24 * 3600 * 1000);
 let $bookingCard = findParent( $container, "booking-card" )
-let $numberDays = $bookingCard.querySelector(".booking-card__number-of-days");
-let lastWord = ( numberDays > 1 ) ? " суток" : " сутки";
-$numberDays.innerText = numberDays + lastWord;
+let $numberDays = $bookingCard.querySelector( ".booking-card__number-of-days" );
+$numberDays.innerText = numberDays + getLastWord( numberDays );
 
 let pricePerDay = $bookingCard.querySelector(
   ".booking-card__price-per-day > .booking-card__price"
@@ -42,10 +63,9 @@ let pricePerDay = $bookingCard.querySelector(
 
 pricePerDay = +pricePerDay.replace(" ", "")
 
-let result = (pricePerDay * numberDays);
+let result = ( pricePerDay * numberDays );
 let $result = $bookingCard.querySelector( ".bookung-card__right > .booking-card__price" );
 $result.innerText = getFormattedPrice( result.toString() );
-
 
 // считаем общую стоимость с учетом скидок и сборов
 let discount = $bookingCard.querySelector( 
@@ -58,7 +78,6 @@ let serviceCharge = $bookingCard.querySelector(
 ).innerText;
 serviceCharge = +serviceCharge.replace( " ", "" );
 
-
 let additionalServiceCharge = $bookingCard.querySelector(
   ".booking-card__additionalServiceCharge > .booking-card__price"
 ).innerText;
@@ -70,3 +89,39 @@ let $total = $bookingCard.querySelector(
   ".booking-card__bottom .booking-card__price" 
 );
 $total.innerText = getFormattedPrice( total.toString() );
+
+const handleDatePickerBtnApplyClick = () => {
+  numberDays = ( dp.selectedDates[1] - dp.selectedDates[0] ) / ( 24 * 3600 * 1000);
+  $numberDays.innerText = numberDays + getLastWord( numberDays );
+  
+  recalculatePrice();
+}
+
+let pricePerPerson = pricePerDay / getNumberGuests();
+
+const handleDropdownBtnApplyClick = () => {
+  if ( dp.selectedDates.length !== 2 ) return;
+  
+  let numberGuests = getNumberGuests();
+  pricePerDay = pricePerPerson * numberGuests;
+  let $pricePerDay = $bookingCard.querySelector(
+    ".booking-card__price-per-day > .booking-card__price"
+  );
+  $pricePerDay.innerText = pricePerDay;
+
+  $bookingCard.querySelector( 
+    ".booking-card__formula > .booking-card__price"
+  ).innerText = pricePerDay;
+
+  recalculatePrice();
+}
+
+addHandler(
+  [ $bookingCard.querySelector(".air-datepicker-buttons").lastElementChild ],
+  handleDatePickerBtnApplyClick
+);
+
+addHandler(
+  [ $bookingCard.querySelector(".dropdown__button-apply") ],
+  handleDropdownBtnApplyClick
+);
