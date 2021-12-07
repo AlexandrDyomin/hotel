c6fb7216a7467c58ccd79f8de2abbec4af1872f4
Alexandr Dyomin <alexandr.dyomin91@gmail.com>
2021-12-07 14:46:08 +0900

    Реализовал смену фона у Landing page

--- ui_kit/src/index.js
+++ ui_kit/src/index.js
@@ -2,5 +2,25 @@ import "./layout/layout.js"
 import "./blocks/header/header.js";
 import "./blocks/footer/footer.js";
 import "./blocks/search-card/search-card.js";
-import "../public/index.scss";
-import "../public/common-style.scss";
+import "../public/pages/landing-page/index.scss";
+import "../public/pages/common-style.scss";
+
+let bgImages = [ 
+  require( "../public/pages/landing-page/landing-bg1-min.jpg"),
+  require( "../public/pages/landing-page/landing-bg2-min.jpg"),
+  require( "../public/pages/landing-page/landing-bg3-min.jpg") 
+];
+
+let bgIndex = 0;
+let bgInterval = 7000;
+
+let bgElement = document.querySelector('.content');
+function bgChange() {
+  bgElement.style.backgroundImage = 'url(' + bgImages[bgIndex] + ')';
+  bgIndex++;
+  if (bgIndex >= bgImages.length) {
+    bgIndex = 0;
+  }
+}
+bgChange();
+setInterval(bgChange, bgInterval);
